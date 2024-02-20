5e8bb413802f33f2df175b589d299d3a975f6f7d
Shiva Ganesh Reddy Linga <131400523+Shiva7ganesh@users.noreply.github.com>
2024-02-20 11:00:14 +0530

    Added Firebase to posts pages

--- lib/FoundItemsPage.dart
+++ lib/FoundItemsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_speed_dial/simple_speed_dial.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'AddFoundItemPage.dart'; // Import your AddFoundItemPage widget here
 import 'AddLostItemPage.dart'; // Import your AddLostItemPage widget here
 import 'package:lostandfound/FoundItemDetailsPage.dart';
@@ -11,50 +12,98 @@ class FoundItemsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Found Items'),
-          automaticallyImplyLeading: false// Update the title here
+        automaticallyImplyLeading: false, // Update the title here
       ),
-      body: ListView.builder(
-        itemCount: 10, // Change this to the actual number of found items
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              // Navigate to FoundItemDetailsPage
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => FoundItemDetailsPage()),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('found_items').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var item = snapshot.data!.docs[index];
+              var itemName = item['itemName'];
+              var itemDescription = item['description'];
+              var imageUrl = item['images'] != null && item['images'].isNotEmpty
+                  ? item['images'][0]
+                  : null; // Assuming images are stored as an array
+
+              return GestureDetector(
+                onTap: () {
+                  // Navigate to FoundItemDetailsPage
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FoundItemDetailsPage()),
+                  );
+                },
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: double.infinity,
+                          height: 200, // Increase the image size here
+                          decoration: BoxDecoration(
+                            color: Colors.grey, // Change the color here
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 1, // Square image
+                            child: Stack(
+                              children: [
+                                if (imageUrl != null)
+                                  Image.network(
+                                    imageUrl,
+                                    fit: BoxFit.cover,
+                                    loadingBuilder: (context, child, loadingProgress) {
+                                      return AnimatedSwitcher(
+                                        duration: Duration(milliseconds: 300),
+                                        child: loadingProgress == null
+                                            ? child
+                                            : Center(
+                                          child: CircularProgressIndicator(),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                if (imageUrl == null)
+                                  Center(
+                                    child: Text(
+                                      'Image not available',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        Text(
+                          itemName ?? '',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          itemDescription ?? '',
+                          style: TextStyle(color: Colors.grey, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               );
             },
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: double.infinity,
-                      height: 200, // Increase the image size here
-                      child: AspectRatio(
-                        aspectRatio: 1, // Square image
-                        child: Container(
-                          color: Colors.grey, // Replace with your image widget or network image
-                          // child: Image.asset('assets/image_placeholder.jpg', fit: BoxFit.cover),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Found Item ${index + 1}', // Update the text here
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Description of found item ${index + 1}', // Update the text here
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-            ),
           );
         },
       ),
--- lib/LostItemsPage.dart
+++ lib/LostItemsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:simple_speed_dial/simple_speed_dial.dart';
 import 'AddFoundItemPage.dart'; // Import your AddFoundItemPage widget here
 import 'AddLostItemPage.dart'; // Import your AddLostItemPage widget here
@@ -11,50 +12,90 @@ class LostItemsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lost Items'),
-          automaticallyImplyLeading: false// Update the title here
+        automaticallyImplyLeading: false,
       ),
-      body: ListView.builder(
-        itemCount: 10, // Change this to the actual number of lost items
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              // Navigate to LostItemDetailsPage
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LostItemDetailsPage()),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('lost_items').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var item = snapshot.data!.docs[index];
+              var itemName = item['itemName'] ?? 'No Name';
+              var itemDescription = item['description'] ?? 'No Description';
+              var images = item['images'] ?? [];
+              var imageUrl = images.isNotEmpty ? images[0] : null;
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LostItemDetailsPage()),
+                  );
+                },
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: double.infinity,
+                          height: 200,
+                          child: AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: Stack(
+                              children: [
+                                if (imageUrl != null)
+                                  Image.network(
+                                    imageUrl,
+                                    fit: BoxFit.cover,
+                                    loadingBuilder: (context, child, loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return child;
+                                      } else {
+                                        return Center(child: CircularProgressIndicator());
+                                      }
+                                    },
+                                  ),
+                                if (imageUrl == null)
+                                  Center(
+                                    child: Text(
+                                      'Image not available',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        Text(
+                          itemName,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          itemDescription,
+                          style: TextStyle(color: Colors.grey, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               );
             },
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      width: double.infinity,
-                      height: 200, // Increase the image size here
-                      child: AspectRatio(
-                        aspectRatio: 1, // Square image
-                        child: Container(
-                          color: Colors.grey, // Replace with your image widget or network image
-                          // child: Image.asset('assets/image_placeholder.jpg', fit: BoxFit.cover),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Text(
-                      'Lost Item ${index + 1}', // Update the text here
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      'Description of lost item ${index + 1}', // Update the text here
-                      style: TextStyle(color: Colors.grey, fontSize: 16),
-                    ),
-                  ],
-                ),
-              ),
-            ),
           );
         },
       ),
@@ -67,17 +108,17 @@ class LostItemsPage extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => AddFoundItemPage()), // Navigate to AddFoundItemPage
+                MaterialPageRoute(builder: (context) => AddFoundItemPage()),
               );
             },
           ),
           SpeedDialChild(
-            child: const Icon(Icons.remove), // Change the icon to minus
+            child: const Icon(Icons.remove),
             label: 'Add Item Lost',
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => AddLostItemPage()), // Navigate to AddLostItemPage
+                MaterialPageRoute(builder: (context) => AddLostItemPage()),
               );
             },
           ),
